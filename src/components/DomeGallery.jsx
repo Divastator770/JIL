05bcd5d4a2d0905d18ec97f7eedee3cec8c2d234
Miraliev Hasan <miralievhasan347@gmail.com>
2025-12-16 19:22:19 +0500

    fourth

--- src/components/DomeGallery.jsx
+++ src/components/DomeGallery.jsx
@@ -2,23 +2,23 @@ import { useEffect, useMemo, useRef, useCallback } from 'react';
 import { useGesture } from '@use-gesture/react';
 
 // Import all images
-import img1 from '../img/1.jpg';
-import img2 from '../img/2.jpg';
-import img3 from '../img/3.jpg';
-import img4 from '../img/4.jpg';
-import img5 from '../img/5.jpg';
-import img6 from '../img/6.jpg';
-import img7 from '../img/7.jpg';
-import img8 from '../img/8.jpg';
-import img9 from '../img/9.jpg';
-import img10 from '../img/10.jpg';
-import img11 from '../img/11.jpg';
-import img12 from '../img/12.jpg';
-import img13 from '../img/13.jpg';
-import img14 from '../img/14.jpg';
-import img15 from '../img/15.jpg';
-import img16 from '../img/16.jpg';
-import img17 from '../img/17.jpg';
+import img1 from '@/img/1.jpg';
+import img2 from '@/img/2.jpg';
+import img3 from '@/img/3.jpg';
+import img4 from '@/img/4.jpg';
+import img5 from '@/img/5.jpg';
+import img6 from '@/img/6.jpg';
+import img7 from '@/img/7.jpg';
+import img8 from '@/img/8.jpg';
+import img9 from '@/img/9.jpg';
+import img10 from '@/img/10.jpg';
+import img11 from '@/img/11.jpg';
+import img12 from '@/img/12.jpg';
+import img13 from '@/img/13.jpg';
+import img14 from '@/img/14.jpg';
+import img15 from '@/img/15.jpg';
+import img16 from '@/img/16.jpg';
+import img17 from '@/img/17.jpg';
 
 const DEFAULT_IMAGES = [
   { src: img1, alt: 'Abstract art' },
